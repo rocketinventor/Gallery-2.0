df2472e698754878d46db6cd7744aa9c9f3bddfb
Elliot Gerchak <e@dementedlab.com>
2015-09-22 19:55:20 +0000

    prevent a specific js error from happening
    Due to laziness, certain math was invalid sometimes but, done anyways. Now it is only done when valid.

--- PP 4.1_files/impress.js
+++ PP 4.1_files/impress.js
@@ -306,18 +306,19 @@ function startPres(document, window) {
         //switch from thumbnail image to full res
         //for current image, and before image, and after too
         //the URL's are from the ajax.js generated array
-        for (var x = -1; x <= 1; x++) {
-          //get step #from url
-          //it's current state is a hack to get the slide #
-          //it will have to be rewritten to get the slide id internally (from variable)
-          //also on the overview, I want it to switch back to thumnail sized images
-          var i = parseInt(window.location.hash.replace("#/painting/step-", ""), 0) - 1 + x;
-          document.getElementById("P" + (i + 1)).style.backgroundImage = 'url(' + images[i].imageURL + ')';
-          //set height and width
-          document.getElementById("P" + (i+ 1)).style.height = images[i].height;
-          document.getElementById("P" + (i+ 1)).style.width = images[i].width;
-        }
-
+       if (location.hash != "#/painting/0") {
+         for (var x = -1; x <= 1; x++) {
+           //get step #from url
+           //it's current state is a hack to get the slide #
+           //it will have to be rewritten to get the slide id internally (from variable)
+           //also on the overview, I want it to switch back to thumnail sized images
+           var i = parseInt(window.location.hash.replace("#/painting/step-", ""), 0) - 1 + x;
+           document.getElementById("P" + (i + 1)).style.backgroundImage = 'url(' + images[i].imageURL + ')';
+           //set height and width
+           document.getElementById("P" + (i + 1)).style.height = images[i].height;
+           document.getElementById("P" + (i + 1)).style.width = images[i].width;
+         }
+       }
       };
 
       // `onStepLeave` is called whenever the step element is left
